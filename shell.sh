e3ed65d31ebb4fcee0d67d7f0faaaafb88d42587
shank <shashank3419@gmail.com>
2019-05-14 17:44:15 +0100

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -5,3 +5,8 @@ tag=$(git log -1 --pretty=format:%h)
 docker build -t ngregistry.imanagelabs.dev/nodeapps:$tag .
 
 docker push ngregistry.imanagelabs.dev/nodeapps:$tag
+
+echo ngregistry.imanagelabs.dev/nodeapps:$tag > imagename 
+
+docker rmi ngregistry.imanagelabs.dev/nodeapps:$tag
+
